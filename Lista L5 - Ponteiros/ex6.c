5cc5768570e22184440f4e83d8cd892ca75c3604
Pedro <phlmesquita@inf.ufpel.edu.br>
2023-07-24 11:50:57 -0300

    só ajustando

--- Lista L5 - Ponteiros/ex6.c
+++ Lista L5 - Ponteiros/ex6.c
@@ -3,7 +3,7 @@
 //Exercício 6. O que fazem os seguintes programas?
 
 int main(){
-  	int vet[] = {4,9,13};  /Declarando um vetor 'vet[]' preenchendo com valores iniciais {4, 9, 10}.
+  	int vet[] = {4,9,13};  //Declarando um vetor 'vet[]' preenchendo com valores iniciais {4, 9, 10}.
 
   	for(int i = 0; i < 3; i++){ //Imprime o conteúdo do bloco da memória cujo o endereço é o do vetor 'vet'.
    	    printf("%d ",*(vet+i));
@@ -18,7 +18,7 @@ void programa2(){
  	int vet[] = {4,9,13};
   	
     for(int i = 0; i < 3 ; i++){
-  	    printf("%d ",vet+i);
+  	    printf("%d ", vet+i);
  	}
     /*Invés de imprimir o conteúdo dos blocos de memória, o programa vai imprimir o 
     endereço dos blocos do vetor 'vet'. É possível ver a variação de 4 bytes 
